e11bdb4d0ff14d568f7ea13feb14e2d208855524
Jack-42 <johnjack0987@gmail.com>
2024-09-23 16:45:26 -0600

    have ChemPage display a table with mol + scaffold + properties

--- src/components/ChemPage.tsx
+++ src/components/ChemPage.tsx
@@ -3,8 +3,52 @@ import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import MoleculeStructure from "./MoleculeStructure.tsx";
 
+// define interfaces
+interface Scaffold {
+    scafsmi: string;
+    pscore: number;
+    in_db: boolean;
+    in_drug: boolean;
+}
 
-export default function ChemPage(props: { result: Object, setChem: Dispatch<SetStateAction<any>> }) {
+
+interface ChemPageProps {
+    result: {
+        molecule_cansmi: string;
+        scaffolds: Scaffold[];
+    };
+    setChem: Dispatch<SetStateAction<any>>;
+}
+
+
+
+// for visualizing score advisory color
+const getRowClassName = (pscore: number) => {
+    if (pscore >= 300) return "bg-red-300";
+    if (pscore >= 100) return "bg-yellow-300";
+    if (pscore >= 0) return "bg-green-300";
+    return "bg-gray-300";
+};
+
+export default function ChemPage(props: ChemPageProps) {
+    const moleculeStructure = (
+        <MoleculeStructure
+            id={`mol-smile-svg`}
+            structure={props.result.molecule_cansmi}
+            width={350}
+            height={300}
+            svgMode={true}
+            className="mb-4"
+        />
+    );
+
+    // Sort the scaffolds array by pscore in descending order
+    const sortedScaffolds = props.result.scaffolds.sort((a, b) => b.pscore - a.pscore);
+
+    // Get the highest pscore and its corresponding row class name (for molecule column color)
+    const highestPscore = sortedScaffolds.length > 0 ? sortedScaffolds[0].pscore : -1;
+    const highestPscoreRowClassName = getRowClassName(highestPscore);
+
     return (
         <div id="chem-page" className="relative z-10">
             <button onClick={() => {
@@ -14,40 +58,48 @@ export default function ChemPage(props: { result: Object, setChem: Dispatch<SetS
                 <span>Back</span>
             </button>
             <div className="glass-container active p-3">
-                <header className="mb-4">
-                    <h1 className="text-3xl font-bold mb-2">{"Input Molecule"}</h1>
-                    <header className="mb-2">
-                        <h2 className="text-xl font-semibold">{"Canonical SMILES: " + props.result.molecule_cansmi}</h2>
-                    </header>
-                    <MoleculeStructure
-                        id={`mol-smile-svg`}
-                        structure={props.result.molecule_cansmi}
-                        width={350}
-                        height={300}
-                        svgMode={true}
-                        className="mb-4"
-                    />
-                </header>
                 <section>
-                    <h1 className="text-3xl font-bold mb-2">{"Scaffolds"}</h1>
-                    {Array.isArray(props.result.scaffolds) && props.result.scaffolds.map((item, index) =>
-                     (
-                        <div key={index} className="mb-4">
-                            <header className="mb-2">
-                                <h2 className="text-xl font-semibold">{"Canonical SMILES: " + item}</h2>
-                            </header>
-                            <MoleculeStructure
-                                id={`scaf-smile-svg-${index}`}
-                                structure={item}
-                                width={350}
-                                height={300}
-                                svgMode={true}
-                                className="mb-4"
-                            />
-                        </div>
-                    ))}
+                    {Array.isArray(props.result.scaffolds) && (
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Molecule</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Scaffold</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">InDrug</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pscore</th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {sortedScaffolds.map((scaffold, index) => {
+                                    const { scafsmi, pscore, in_db, in_drug } = scaffold;
+                                    const inDrugString = !in_db ? "NULL" : (in_drug ? "True" : "False");
+                                    const pscoreString = !in_db ? "NULL" : pscore;
+                                    const rowClassName = getRowClassName(pscore);
+                                    return (
+                                        <tr key={index} className={rowClassName}>
+                                            <td className={`px-6 py-4 whitespace-nowrap border-r border-gray-200 ${highestPscoreRowClassName}`}>
+                                                {moleculeStructure}
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap border-r border-gray-200">
+                                                <MoleculeStructure
+                                                    id={`scaf-smile-svg-${index}`}
+                                                    structure={scafsmi}
+                                                    width={350}
+                                                    height={300}
+                                                    svgMode={true}
+                                                    className="mb-4"
+                                                />
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 border-r border-gray-200">{inDrugString}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{pscoreString}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    )}
                 </section>
             </div>
         </div>
-    )
+    );
 }
