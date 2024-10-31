6ac3b5c4de41e04dfb7495752589ceae5b2acc7b
Jack-42 <johnjack0987@gmail.com>
2024-10-31 16:11:21 -0600

    update logos, rm unused background

--- replace_logo.sh
+++ replace_logo.sh
@@ -1,6 +1,7 @@
 # file to recursively replace logos under "favicon" with new logo
 
 SVG_LOGO="public/badapple_logo.svg"
+SVG_LOGO_NO_TEXT="public/badapple_logo_no_text.svg"
 
 # Directory containing the favicons
 FAVICONS_DIR="public/favicons"
@@ -11,12 +12,12 @@ SIZES=(16 32)
 # Convert SVG to PNG for each size and replace the existing files
 for SIZE in "${SIZES[@]}"; do
     PNG_FILE="${FAVICONS_DIR}/favicon-${SIZE}x${SIZE}.png"
-    rsvg-convert -w $SIZE -h $SIZE -o $PNG_FILE $SVG_LOGO
+    rsvg-convert -w $SIZE -h $SIZE -o $PNG_FILE $SVG_LOGO_NO_TEXT
 done
 
 # Convert favicon.ico file - is 48x48
 FAVICON_FILE="${FAVICONS_DIR}/favicon.ico"
-rsvg-convert -w 48 -h 48 -o $FAVICON_FILE $SVG_LOGO
+rsvg-convert -w 48 -h 48 -o $FAVICON_FILE $SVG_LOGO_NO_TEXT
 
 # Convert apple-touch-icon.png - is 180x180
 APPLE_TOUCH_ICON="${FAVICONS_DIR}/apple-touch-icon.png"
@@ -36,7 +37,7 @@ rsvg-convert -w 150 -h 150 -o $MSTILE_150 $SVG_LOGO
 
 # Convert safari-pinned-tab.svg
 SAFARI_PINNED_TAB="${FAVICONS_DIR}/safari-pinned-tab.svg"
-cp $SVG_LOGO $SAFARI_PINNED_TAB
+cp $SVG_LOGO_NO_TEXT $SAFARI_PINNED_TAB
 
 
 echo "Favicons replaced successfully."
