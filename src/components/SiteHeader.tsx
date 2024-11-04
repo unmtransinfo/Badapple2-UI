14a99b4e74e4c9d3ee6d6e659fdc9cc1fcc7e4bd
Jack-42 <johnjack0987@gmail.com>
2024-11-04 15:11:27 -0700

    remove padding on logo

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,8 +1,8 @@
 export default function SiteHeader() {
     return (
-        <header className="text-center mb-8">
+        <header className="text-center">
             <h1>
-                <img src="/badapple2/badapple_logo.svg" title="Badapple Logo" alt="Badapple Logo" className="inline-block w-36 mb-4" />
+                <img src="/badapple2/badapple_logo.svg" title="Badapple Logo" alt="Badapple Logo" className="inline-block w-36" />
             </h1>
         </header>
     )
