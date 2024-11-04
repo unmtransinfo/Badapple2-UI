1dcf5802fee97ab706c5ab5612a3d3bdb084b27b
Jack-42 <johnjack0987@gmail.com>
2024-11-04 14:34:21 -0700

    make logo smaller

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,7 +2,7 @@ export default function SiteHeader() {
     return (
         <header className="text-center mb-8">
             <h1>
-                <img src="/badapple2/badapple_logo.svg" title="Badapple Logo" alt="Badapple Logo" className="inline-block w-48 mb-4" />
+                <img src="/badapple2/badapple_logo.svg" title="Badapple Logo" alt="Badapple Logo" className="inline-block w-36 mb-4" />
             </h1>
         </header>
     )
