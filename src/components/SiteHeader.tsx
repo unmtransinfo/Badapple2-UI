7b5fb5f36946ff79b1ba04fe75874eccda4267eb
Jack-42 <johnjack0987@gmail.com>
2025-08-28 16:31:29 -0600

    update logo

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -8,7 +8,7 @@ const SiteHeader = () => {
           src="/badapple2/badapple_logo.svg"
           title="Badapple Logo"
           alt="Badapple Logo"
-          className="inline-block w-36"
+          className="inline-block h-32"
         />
       </h1>
       <div className="version-banner">Version: 1.0</div>
