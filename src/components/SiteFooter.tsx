fbe2a08875817691aad8b680de512420cc427caf
Jack-42 <johnjack0987@gmail.com>
2025-08-29 11:43:34 -0600

    update UNM logo

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,11 +1,19 @@
 export default function SiteFooter() {
-    return (
-        <footer className="text-center mt-4 mb-0">
-            <h1>
-                <a href="https://datascience.unm.edu/" target="_blank" rel="noopener noreferrer">
-                    <img src="/badapple2/University_of_New_Mexico_logo.svg" title="UNM School of Medicine, Dept. of Internal Medicine, Translational Informatics Division" className="inline-block w-72" />
-                </a>
-            </h1>
-        </footer>
-    )
+  return (
+    <footer className="text-center mt-4 mb-0">
+      <h1>
+        <a
+          href="https://datascience.unm.edu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/badapple2/hsc_logo.png"
+            title="UNM School of Medicine, Dept. of Internal Medicine, Translational Informatics Division"
+            className="inline-block h-20"
+          />
+        </a>
+      </h1>
+    </footer>
+  );
 }
